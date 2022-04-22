ffd6370af795903acaf03e0f4c8fea8254dc2468
Gerard Bourguett <gerard.bourguett@gmail.com>
2022-04-22 15:07:18 -0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes, NavLink } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+import Contacto from "./pages/Contacto";
+import Acerca from "./pages/Acerca";
+import Tabla from "./pages/Tabla";
+import Detalle from "./pages/Detalle";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="container mt-5">
+        <div className="btn-group">
+          <NavLink to="/" className="btn btn-dark">
+            Inicio
+          </NavLink>
+          <NavLink to="/tabla" className="btn btn-dark">
+            Tabla
+          </NavLink>
+          <NavLink to="/contacto" className="btn btn-dark">
+            Contacto
+          </NavLink>
+          <NavLink to="/acerca" className="btn btn-dark">
+            Acerca
+          </NavLink>
+        </div>
+        <hr />
+        <Routes>
+          <Route path="/detalle/:id" element={<Detalle />}></Route>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/tabla" element={<Tabla />}></Route>
+          <Route path="/contacto" element={<Contacto />}></Route>
+          <Route path="/acerca" element={<Acerca />}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
